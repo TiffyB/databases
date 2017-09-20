b98cbab3259d23bd9d6e589971c514f19f659ff1
Hack Reactor Student <student@hackreactor.com>
2017-09-19 19:37:40 -0700

    more code cleanup

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -2,49 +2,37 @@ var models = require('../models');
 
 module.exports = {
   messages: {
-    get: function (req, res) { // a function which handles a get request for all messages for the client
+    get: function (req, res) { 
       models.messages.get(function(messages) {
-        //need to get user name using query
-        // console.log(messages);
         res.end(JSON.stringify(messages));
       });
 
     }, 
-    post: function (req, res) { // a function which handles posting a message to the database 
+    post: function (req, res) { 
 
-      
-      // res.statusCode = 201;
       var messageData = [];
       req.on('data', (chunk) => {
-        // console.log('got to post messages in controllers');
-        // console.log(chunk);
         messageData.push(chunk);
       });
       req.on('end', () => {
         messageData = [].concat(messageData).toString('utf8');
         var parsedMessage = JSON.parse(messageData);
-        //send to models?
         models.messages.post(parsedMessage, function(err, result) {
           res.sendStatus(201);
-        });
-        
+        }); 
       });
-      // res.end(); //does this work? Or do I need to write the headers as well?
     } 
   },
 
   users: {
-    // Ditto as above
     get: function (req, res) {
       console.log('got here');
       models.users.get(function(users) {
-        // console.log("users ", users);
         res.end(JSON.stringify(users));
       });
       
     },
     post: function (req, res) {
-      // res.statusCode = 201;
       var userData = [];
       req.on('data', (chunk) => {
         userData.push(chunk);
@@ -52,14 +40,10 @@ module.exports = {
       req.on('end', () => {
         userData = [].concat(userData).toString('utf8');
         var parsedUser = JSON.parse(userData);
-        //send to models?
         models.users.post(parsedUser, function(err, result) {
           res.sendStatus(201);
         });
-        
       });
-      // res.end(); //does this work? Or do I need to write the headers as well?
-
     }
   }
 };
--- server/models/index.js
+++ server/models/index.js
@@ -3,7 +3,6 @@ var db = require('../db');
 module.exports = {
   messages: {
     get: function (callback) { // a function which produces all the messages
-
       var queryStr = 'SELECT * FROM messages left outer join users on (messages.user = users.userid) order by messages.id desc';
       db.query(queryStr, function(err, results) {
         callback(results);
@@ -11,9 +10,7 @@ module.exports = {
 
     }, 
     post: function (messageData, callback) { // a function which can be used to insert a message into the database
-    
       var sql = "INSERT INTO messages (user, text, roomname) VALUE ((SELECT userid FROM users WHERE name = ? limit 1), ?, ?)";
-
       var values = [messageData.user, messageData.text, messageData.roomname];
       db.query(sql, values, function(err, result) {
         console.log(err);
@@ -27,16 +24,12 @@ module.exports = {
     // Ditto as above.
     get: function (callback) {
       var queryStr = 'SELECT * FROM users';
-      
-      console.log('got to users model');
-      // var query = db.query(queryStr);
       db.query(queryStr, function(err, results) {
         callback(results);
       });
     },
     post: function (userData, callback) {
       var sql = "INSERT INTO users (name) VALUE (?)";
-      
       var values = [userData.name];
       db.query(sql, values, function(err, result) {
         console.log("Number of users inserted: " + result.affectedRows);
